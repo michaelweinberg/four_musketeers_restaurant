de1adc7052426665af3ff516963078ef65c05076
Navy2008 <duartemanuel@hotmail.com>
2022-05-03 09:51:12 -0700

    made changes to: app and html files

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', utc_dt=datetime.datetime.utcnow())
 
 @app.route('/login')
 def about():
