38091e385707d4bc1c2de9d48d1d1a2948b0228d
Michael Weinberg <michaelweinberg@Michaels-MacBook-Pro.local>
2022-05-05 14:10:20 -0700

    index.html rendering from flask endpoint

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', utc_dt=datetime.datetime.utcnow())
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 @app.route('/login')
 def about():
     return render_template('login.html')
 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
