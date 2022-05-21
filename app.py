0c124f47879e8587c771750473c7f2c8fb6745c6
Michael Weinberg <michaelweinberg@Michaels-MacBook-Pro.local>
2022-05-21 11:23:41 -0700

    adding imports and docker file

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash, url_for
+from flask_wtf import FlaskForm
+from wtforms import DateField, DecimalField, SubmitField, StringField, PasswordField
+from wtforms.validators import DataRequired, NumberRange
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+import requests
 import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+SECRET_KEY = 'fehiu4y74gh894hg49t8t484'
+SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
+
 app = Flask(__name__)
+
+# login_manager = LoginManager(app)
+# login_manager.login_view = "login"
+# db = SQLAlchemy(app)
+
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 @app.route('/')
 def home():
