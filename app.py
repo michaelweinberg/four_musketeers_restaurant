210929b0e4a97a694896d14a4e4fa98917fa858a
Michael Weinberg <michaelweinberg@Michaels-MacBook-Pro.local>
2022-05-21 11:48:00 -0700

    adding signup form - linking to sqlite db

--- app.py
+++ app.py
@@ -14,21 +14,76 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
 
 app = Flask(__name__)
 
-# login_manager = LoginManager(app)
-# login_manager.login_view = "login"
-# db = SQLAlchemy(app)
-
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+db = SQLAlchemy(app)
+
+@login_manager.user_loader
+def get_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(90), unique=True, nullable=False)
+    password = db.Column(db.String(90), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = generate_password_hash(password)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class MainAppForm(FlaskForm):
+    birth_date = DateField("date", format='%Y-%m-%d',
+                           validators=[DataRequired()])
+    how_many = DecimalField("how many", validators=[
+        DataRequired(), NumberRange(1, 20)])
+    submit = SubmitField("submit")
+
+
+class LoginForm(FlaskForm):
+    username = StringField("username", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    submit = SubmitField("submit")
+
+
+class SignupForm(FlaskForm):
+    username = StringField("username", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    submit = SubmitField("submit")
+    
+
 @app.route('/')
+@login_required
 def home():
     return render_template('home.html', utc_dt=datetime.datetime.utcnow())
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    print("loggin in")
-    return render_template('login.html')
+    form = LoginForm()
+
+    username = form.username.data
+    password = form.password.data
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user and user.verify_password(password):
+            login_user(user)
+            flash("Logged in successfully")
+            return redirect(url_for("index"))
+        else:
+            flash("Invalid username or password. Please try again")
+            return redirect(url_for("login"))
+    return render_template("login.html", form=form)
 
 @app.route('/menu')
 def menu():
@@ -48,6 +103,27 @@ def about():
 @app.route('/comp')
 def comp():
     return render_template('index.html')
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash("username already in use. Try again with a different username")
+            return redirect(url_for('signup'))
+        else:
+            user = User(username, password)
+            db.session.add(user)
+            db.session.commit()
+            flash("User created successfully")
+            return redirect(url_for('login'))
+
+    return render_template("signup.html", form=form)    
 
 
 if __name__ == "__main__":
